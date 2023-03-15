39ffb0c4b46a21dac6ea42d0006d4a65c73dc27a
bogdan <bogdanhalici1989@gmail.com>
2023-03-15 16:32:48 +0000

    PCM-1 setUp hibernate configuration

---
+++ src/main/java/com/sda/practicalproject/utils/SessionManager.java
@@ -0,0 +1,24 @@
+package com.sda.practicalproject.utils;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class SessionManager extends AbstractSessionManager{
+    private static final SessionManager INSTANCE = new SessionManager();
+    private SessionManager(){
+
+    }
+    public static SessionFactory getSessionFactory()
+    {
+        return INSTANCE.getSessionFactory("pet_clinic");
+    }
+
+    public static void shutdown()
+    {
+        INSTANCE.shutdownSessionManager();
+    }
+    @Override
+    protected void setAnnotatedClasses(Configuration configuration) {
+
+    }
+}
