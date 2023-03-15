ee07d833f421c473aa38ea7235a3b57771e12683
bogdan <bogdanhalici1989@gmail.com>
2023-03-15 18:56:19 +0000

    PCM-3 create generic hibernate repository

---
+++ src/main/java/com/sda/practicalproject/repository/ConsultRepository.java
@@ -0,0 +1,9 @@
+package com.sda.practicalproject.repository;
+
+import com.sda.practicalproject.model.Consult;
+import com.sda.practicalproject.model.Vet;
+import com.sda.practicalproject.repository.base.Repository;
+
+public interface ConsultRepository extends Repository<Consult> {
+
+}
