e5ec7a56318aac8bfc103a82246bfa776795c378
bogdan <bogdanhalici1989@gmail.com>
2023-03-16 16:59:49 +0000

    PCM - 4

--- src/main/java/com/sda/practicalproject/Main.java
+++ src/main/java/com/sda/practicalproject/Main.java
@@ -1,14 +1,53 @@
 package com.sda.practicalproject;
 
+import com.sda.practicalproject.controler.menu.MenuItem;
 import com.sda.practicalproject.utils.SessionManager;
+
+import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         SessionManager.getSessionFactory();
 
+        for (int i = 0; i <= 100; i++) {
+            System.out.println("/////////////////////////////////");
+        }
+        MenuItem selectedOption = MenuItem.UNKNOWN;
+        while (selectedOption != MenuItem.EXIT) {
+            System.out.println();
+            MenuItem.printMenuItems();
+            System.out.println("Please select an option : ");
+            try {
+                int numericOption = Integer.parseInt(scanner.nextLine());
+                selectedOption = MenuItem.searchByOption(numericOption);
+            }catch (NumberFormatException e){
+                System.out.println("Please use numeric value");
+                selectedOption = MenuItem.UNKNOWN;
+            }
 
-
-
+            switch (selectedOption) {
+                case ADD_VET:
+                    System.out.println("Add vet not implemented");
+                    break;
+                case UPDATE_VET:
+                    System.out.println("Update vet not implemented");
+                    break;
+                case DELETE_VET:
+                    System.out.println("Delete vet not implemented");
+                    break;
+                case EXIT:
+                    System.out.println("Good Bye and have a nice day");
+                    break;
+                case UNKNOWN:
+                    System.out.println("Please insert a valid option");
+                    break;
+                default:
+                    System.out.println("Option not implemented");
+                    break;
+            }
+        }
         SessionManager.shutdown();
     }
 }
---
+++ src/main/java/com/sda/practicalproject/controler/menu/MenuItem.java
@@ -0,0 +1,33 @@
+package com.sda.practicalproject.controler.menu;
+
+public enum MenuItem {
+    ADD_VET(1,"Add vet"),
+    UPDATE_VET(2,"Update vet"),
+    DELETE_VET(3,"Delete vet"),
+    VIEW_VET_LIST(4,"View vet list"),
+    EXIT(100,"Exit"),
+    UNKNOWN(999, "Unknown option");
+
+    private final int option;
+    private final String displayName;
+
+    MenuItem(int option, String displayName) {
+        this.option = option;
+        this.displayName = displayName;
+    }
+    public static void printMenuItems() {
+        for (MenuItem value : values()) {
+            if (value != UNKNOWN) {
+                System.out.println(value.option + " -> " + value.displayName);
+            }
+        }
+    }
+    public static MenuItem searchByOption(int inputOption){
+        for(MenuItem value : values()){
+            if(value.option == inputOption){
+                return value;
+            }
+        }
+        return UNKNOWN;
+    }
+}
